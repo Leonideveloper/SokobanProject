699f0847e37b53096b0286ba5c912e371af1af5d
Semenov Leonid <leonidandand@gmail.com>
2013-07-01 22:23:50 +0400

    Тренировка в отображении картинок в виде таблицы

--- Sokoban/src/main/java/com/gmail/leonidandand/sokoban/EditorActivity.java
+++ Sokoban/src/main/java/com/gmail/leonidandand/sokoban/EditorActivity.java
@@ -1,8 +1,19 @@
 package com.gmail.leonidandand.sokoban;
 
+import android.app.ActionBar;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+import java.util.Random;
 
 public class EditorActivity extends Activity {
 
@@ -10,14 +21,45 @@ public class EditorActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        prepareTable();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.editor, menu);
-        return true;
+    private void prepareTable() {
+        final int ROWS = getResources().getInteger(R.integer.rows);
+        final int COLUMNS = getResources().getInteger(R.integer.columns);
+        LinearLayout editorVerticalLayout = (LinearLayout) findViewById(R.id.editorVerticalLayout);
+        for (int row = 0; row < ROWS; ++row) {
+            LinearLayout layout = prepareRow(COLUMNS);
+            editorVerticalLayout.addView(layout);
+        }
     }
-    
+
+    private LinearLayout prepareRow(int columns) {
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.HORIZONTAL);
+        for (int col = 0; col < columns; ++col) {
+            layout.addView(prepareTableElement());
+        }
+        addParams(layout);
+        layout.setBackgroundColor(new Random().nextBoolean() ? Color.BLACK : Color.WHITE);
+        return layout;
+    }
+
+    private View prepareTableElement() {
+        ImageView tableElement = new ImageView(this);
+        tableElement.setImageDrawable(getResources().getDrawable(R.drawable.img));
+        addParams(tableElement);
+        tableElement.setScaleType(ImageView.ScaleType.FIT_XY);
+        return tableElement;
+    }
+
+    private void addParams(View view) {
+        LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                1.0f);
+        view.setLayoutParams(param);
+    }
+
 }
